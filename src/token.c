4527c8c797058a1aab60eb1a0566648ad43ab13b
TN <toby@newtonnet.org>
2025-10-07 23:44:59 +0100

    Print token improvements

--- src/token.c
+++ src/token.c
@@ -1,7 +1,6 @@
 #include "token.h"
 
 #include <stdio.h>
-#include <string.h>
 
 #include "identifier_table.h"
 
@@ -77,12 +76,9 @@ void printToken(Token token) {
 			tokenTypeToString(token.type), token.offset_in_source, token.length_in_source, token.line_number, token.column_number);
 	
 	switch (token.type) {
-		case TOKEN_IDENTIFIER: printf(", identifier ID: %zu, identifier name: ", token.data.identifier_ID);
+		case TOKEN_IDENTIFIER:;
 		Identifier identifier = getIdentifierFromID(token.data.identifier_ID);
-		for (size_t i = 0; i < identifier.name_length; ++i) {
-			putchar(identifier.name[i]);
-		}
-		printf("}");
+		printf(", identifier ID: %zu, identifier name: %.*s}", token.data.identifier_ID, (int)identifier.name_length, identifier.name);
 		break;
 
 		case TOKEN_INTEGER_TYPE:
@@ -94,13 +90,7 @@ void printToken(Token token) {
 		case TOKEN_INTEGER_LITERAL: printf(", data: %zu}", token.data.integer); break;
 		case TOKEN_REAL_LITERAL: printf(", data: %f}", token.data.real); break;
 		case TOKEN_CHARACTER_LITERAL: printf(", data: %c}", token.data.character); break;
-		case TOKEN_STRING_LITERAL:
-		printf(", data: ");
-		for (size_t i = 0; i < token.data.string.length; ++i) {
-			putchar(token.data.string.text[i]);
-		}
-		printf("}");
-		break;
+		case TOKEN_STRING_LITERAL: printf(", data: %.*s}", (int)token.data.string.length, token.data.string.text); break;
 
 		default: printf("}"); break;
 
